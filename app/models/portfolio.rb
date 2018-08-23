0095989c345cac580acafda3e5c3db00a0ef8b76
joyytoo <joytoo97@gmail.com>
2018-08-23 10:23:04 +0300

    feat(dataManagement): implement custom scope for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,9 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  # def self.react
+  #   where(subtitle: 'React JS')
+  # end
+  scope :react, -> { where(subtitle: 'React JS') }
+
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts '3 topics created'
+
 10.times do |blog|
   Blog.create!(
     title: "My blog Post #{blog}",
@@ -12,7 +20,8 @@
             remaining essentially unchanged. It was popularised in the 1960s
             with the release of Letraset sheets containing Lorem Ipsum passages,
             and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.'
+            PageMaker including versions of Lorem Ipsum.',
+    topic_id: Topic.last.id
   )
 end
 
@@ -27,14 +36,27 @@ end
 
 puts '5 skills created'
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio Title: #{portfolio_item}",
-    subtitle: 'My great service',
+    subtitle: 'Ruby on Rails',
     body: ' Duis aute irure dolor in reprehenderit
             in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum."',
+    main_image: 'https://via.placeholder.com/600x400',
+    thumb_image: 'https://via.placeholder.com/300x200'
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Title: #{portfolio_item}",
+    subtitle: 'React JS',
+    body: ' Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+          officia deserunt mollit anim id est laborum."',
     main_image: 'https://via.placeholder.com/600x400',
     thumb_image: 'https://via.placeholder.com/300x200'
   )
