9f4006dd6f0cc82b8187af0b5a79882f0c380b82
Joy Too <joytoo97@gmail.com>
2018-10-23 20:14:29 +0300

    Views (#13)
    * feat(views): add navbar and default header content
    
    * feat(views): add custom layouts for blogs and portfolios
    
    * feat(views): implement partials for nav components
    
    * feat(views): implement partials with local data for portfolio form and nav bars
    
    * feat(views): implement view helper for auth links
    
    * feat(views): implement content tag helper methods
    
    * feat(views): implement collection based partials
    
    * feat(views): integrate helper methods from ActionView

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
+# view helpers are used to store additional conditional logic
+# where majority is ruby code
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      link_to 'Logout', destroy_user_session_path, method: :delete
+    else
+      # concatenate for both items to be displayed after else
+      (link_to 'Register', new_user_registration_path) + '<br>'.html_safe +
+        (link_to 'Login', new_user_session_path)
+    end
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thank you for visiting from me #{session[:source]}
+      and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: 'source-greeting')
+    end
+  end
 end
+
+# html_safe sanitizes so it is displayed as html instead of a string
+# fine to use it when not dealing with user input content
