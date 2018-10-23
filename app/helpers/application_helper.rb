f1fe5a367c589b22907449cf58834fea9d9ed075
joyytoo <joytoo97@gmail.com>
2018-10-23 19:16:16 +0300

    feat(views): implement content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,15 @@ module ApplicationHelper
     else
       # concatenate for both items to be displayed after else
       (link_to 'Register', new_user_registration_path) + '<br>'.html_safe +
-      (link_to 'Login', new_user_session_path)
+        (link_to 'Login', new_user_session_path)
+    end
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thank you for visiting from me #{session[:source]}
+      and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: 'source-greeting')
     end
   end
 end
