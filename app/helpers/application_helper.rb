45d75a0e2e6b6304b29f6edefecf081835fde78d
joyytoo <joytoo97@gmail.com>
2018-10-23 18:49:45 +0300

    feat(views): implement view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# view helpers are used to store additional conditional logic
+# where majority is ruby code
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      link_to 'Logout', destroy_user_session_path, method: :delete
+    else
+      # concatenate for both items to be displayed after else
+      (link_to 'Register', new_user_registration_path) + '<br>'.html_safe +
+      (link_to 'Login', new_user_session_path)
+    end
+  end
 end
+
+# html_safe sanitizes so it is displayed as html instead of a string
+# fine to use it when not dealing with user input content
