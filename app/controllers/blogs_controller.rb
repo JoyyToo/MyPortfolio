08055d32fbbf2ee5a71cb6e74a8fca78b5df4553
joyytoo <joytoo97@gmail.com>
2018-10-26 19:40:56 +0300

    update auth to include toggle status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy toggle_status]
   layout 'blog'
   access all: %i[show index], user:
-      { except: %i[destroy new create edit update] }, site_admin: :all
+      { except: %i[destroy new create edit update toggle_status] }, site_admin: :all
 
   def index
     @blogs = Blog.special_blogs
