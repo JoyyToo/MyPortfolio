030be655ac4e3c8cd48531d7d6f61791ba1b40f9
joyytoo <joytoo97@gmail.com>
2018-10-26 12:35:07 +0300

    feat(authorization): implement authorization engine for portfolio and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@
 # where majority is ruby code
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-      link_to 'Logout', destroy_user_session_path, method: :delete
-    else
-      # concatenate for both items to be displayed after else
+    if current_user.is_a?(GuestUser)
+      # concatenate for both items to be displayed
       (link_to 'Register', new_user_registration_path) + '<br>'.html_safe +
-        (link_to 'Login', new_user_session_path)
+          (link_to 'Login', new_user_session_path)
+    else
+      link_to 'Logout', destroy_user_session_path, method: :delete
     end
   end
 
