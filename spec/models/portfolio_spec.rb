3062bb29e635c48130a3f65ace3840da18852484
joyytoo <joytoo97@gmail.com>
2018-11-01 00:02:41 +0300

    feat(imagemgt): update file upload forms and refactored placeholder functionality to views

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -5,7 +5,5 @@ RSpec.describe Portfolio, :type => :model do
     it { should have_many(:technologies) }
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
-    it { should validate_presence_of(:main_image) }
-    it { should validate_presence_of(:thumb_image) }
   end
 end
