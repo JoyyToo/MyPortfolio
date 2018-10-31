f365058955ec2160fcca90d6eb80b95fec9f4378
joyytoo <joytoo97@gmail.com>
2018-10-31 23:14:16 +0300

    feat(imagemgt): finish carrierwave installation

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,8 +1,12 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
-  storage :file
+  storage :aws
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def download_url(filename)
+    url(response_content_disposition: %Q{attachment; filename="#{filename}"})
   end
 
   def extension_whitelist
