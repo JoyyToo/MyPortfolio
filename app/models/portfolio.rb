d2c0fb4440aaf0a9e8c8b1277c7e6db96241f9b2
joyytoo <joytoo97@gmail.com>
2018-11-01 00:02:41 +0300

    feat(imagemgt): update file upload forms and refactored placeholder functionality to views

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,5 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
   has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
@@ -19,10 +18,4 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '300', width: '200')
-  end
 end
