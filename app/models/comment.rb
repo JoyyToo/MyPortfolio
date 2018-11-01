332d72366b30b0cbd91d4c255014ed77493a06ff
joyytoo <joytoo97@gmail.com>
2018-11-01 16:53:48 +0300

    Fix failing tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+
+  validates_presence_of :content
+
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    content { "MyText" }
-    user { nil }
-    blog { nil }
+    content { Faker::Lorem.paragraph }
+    user { User.create!(name: Faker::Internet.name, email: Faker::Internet.email, password: Faker::Internet.password) }
+    blog { Blog.create!(title: Faker::Lorem.title, body: Faker::Lorem.paragraph) }
   end
 end
