69c0b3e185aa91692254ff47966463fea4dd959f
joyytoo <joytoo97@gmail.com>
2018-11-06 17:24:30 +0300

    feat(actioncable): Implement action cable for comments

--- app/models/guest_user.rb
+++ app/models/guest_user.rb
@@ -1,4 +1,4 @@
 class GuestUser < User
-  attr_accessor :name, :first_name, :last_name, :email
+  attr_accessor :name, :first_name, :last_name, :email, :id
   validates_presence_of :name, :email
 end
